9b28a437d44eb06821f9789c199a4c16b86bc112
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-02-16 09:19:28 +0530

    Update live_meeting.php

--- application/views/student/live_meeting.php
+++ application/views/student/live_meeting.php
@@ -75,7 +75,11 @@ if($this->session->flashdata('meeting_over')){
 </div>
 
 
-<!-- added live preview with details-->
+<!-- added live preview with details
+      meeting progress added
+
+
+-->
 
 
 
