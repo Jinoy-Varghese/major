30beb8d3cae0c3cd9344c408933a7363c084278a
Arun-Ayyappan <77910661+Arun-Ayyappan@users.noreply.github.com>
2021-03-10 23:10:58 +0530

    hod edit

--- application/controllers/Hod.php
+++ application/controllers/Hod.php
@@ -24,6 +24,7 @@ class Hod extends CI_Controller
   public function __construct()
   {
     parent::__construct();
+    $this->load->model('Hod_model');
   }
 
   public function index()
@@ -224,7 +225,78 @@ class Hod extends CI_Controller
     redirect('hod/add_student','refresh');
   }
 
-
+  public function Hod_profile()
+  {
+    $this->load->view("header.php");
+    $this->load->view("Hod/dash_head.php");
+    $this->load->view("Hod/my_profile4.php");
+    $this->load->view("Hod/dash_footer.php");
+    $this->load->view("footer.php");
+  }
+  
+  public function update_profile()
+    {
+    if($this->input->post('update_user'))
+    {
+     $id=$this->input->post('id');; 
+     $name=$this->input->post('name');
+     $email=$this->input->post('email');
+     $address=$this->input->post('address');  
+     $gender=$this->input->post('gender');
+     $phone=$this->input->post('phone');
+     $update_data=array('name'=>$name,'email'=>$email,'address'=>$address,'gender'=>$gender,'phone'=>$phone);
+     $this->Hod_model->update_profile($update_data,$id);
+     $this->session->set_flashdata('update_success',"Successfully Updated");
+     redirect('Hod/Hod_profile','refresh');
+    }
+    else
+    {
+      $this->session->set_flashdata('update_failed',"Updation Failed");
+      redirect('Hod/Hod_profile','refresh');
+    }
+   }
+  
+   public function change_password()
+   {
+     if($this->input->post('changepw_btn'))
+     {
+     $id=$this->input->post('id');;
+     $current=$this->input->post('current');
+     $new=$this->input->post('new');
+     $confirm=$this->input->post('confirm');
+  
+      $sql=$this->db->get_where('users',array('email'=>$_SESSION["u_id"]));
+      foreach($sql->result() as $user_details)
+      {
+        $password=$user_details->password;
+      } 
+        if($password==md5($current))
+        {
+          if(md5($new)==md5($confirm))
+          {
+            $update_password=array('password'=>md5($confirm));
+            $this->Create_user_model->password_change($update_password,$id);
+            $this->session->set_flashdata('changepass_success',"Password Changed Successfully");
+            redirect('Hod/Hod_profile','refresh');
+          }
+          else
+          {
+            $this->session->set_flashdata('changepass_failed',"New Password & Confirm Password Mismatch...!");
+            redirect('Hod/Hod_profile','refresh');
+          }
+        }
+        else
+        {
+          $this->session->set_flashdata('changepass_old_failed',"Current Password Mismatch...!");
+          redirect('Hod/Hod_profile','refresh');
+        }
+   }
+   else
+   {
+     $this->session->set_flashdata('changepass_wrong',"Password is wrong...!");
+     redirect('Hod/Hod_profile','refresh');
+   }
+  }
 
 
 
