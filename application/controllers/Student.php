8363c236001251a991e83dcdc4128361298fdc69
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-03-10 10:20:59 +0530

    Update Student.php

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -93,18 +93,18 @@ public function update_profile()
           $update_password=array('password'=>md5($confirm));
           $this->Create_user_model->password_change($update_password,$id);
           $this->session->set_flashdata('changepass_success',"Password Changed Successfully");
-          redirect('Student/sudent_profile','refresh');
+          redirect('Student/student_profile','refresh');
         }
         else
         {
           $this->session->set_flashdata('changepass_failed',"New Password & Confirm Password Mismatch...!");
-          redirect('Student/sudent_profile','refresh');
+          redirect('Student/student_profile','refresh');
         }
       }
       else
       {
         $this->session->set_flashdata('changepass_old_failed',"Current Password Mismatch...!");
-        redirect('Student/sudent_profile','refresh');
+        redirect('Student/student_profile','refresh');
       }
  }
  else
