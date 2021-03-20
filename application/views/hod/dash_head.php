5e3d9887539a8ec484285d75aa67ab9b1620dc4a
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-03-20 21:16:22 +0530

    Update dash_head.php

--- application/views/hod/dash_head.php
+++ application/views/hod/dash_head.php
@@ -82,7 +82,7 @@ if(!isset($_SESSION['u_id']))
         <a href="<?php echo base_url('/hod/change_incharge'); ?>" class="list-group-item list-group-item-action bg-light">Add Incharge</a>
         <a href="<?php echo base_url('/hod/add_course'); ?>" class="list-group-item list-group-item-action bg-light">Add Course</a> 
         <a href="<?php echo base_url('/hod/add_subject'); ?>" class="list-group-item list-group-item-action bg-light">Add Subject</a> 
-        <a href="<?php echo base_url('/hod/assign_teachers'); ?>" class="list-group-item list-group-item-action bg-light" data-toggle="tooltip" data-placement="bottom" title="Assign professors for each subject">Assign Teachers</a> 
+        <a href="<?php echo base_url('/hod/assign_teachers'); ?>" class="list-group-item list-group-item-action bg-light" data-toggle="tooltip" data-placement="bottom" title="Assign professors for each subject">Assign Subject</a> 
 
 
         
