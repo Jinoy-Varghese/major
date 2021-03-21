ad144abaaffdd4f1a2db47d8864d6691ae58c2b8
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-03-21 14:19:23 +0530

    added principal

---
+++ application/views/principal/dash_head.php
@@ -0,0 +1,114 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+if(!isset($_SESSION['u_id']))
+{
+  redirect('Home/login','refresh');
+}
+
+?>
+<style>
+#navbarDropdownMenuLink::after
+{
+
+}
+
+
+</style>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="<?php echo base_url('assets/js/sweetalert2.all.min.js'); ?>"></script>
+<?php 
+  $sql=$this->db->get_where('users',array('email'=>$_SESSION["u_id"]));
+  foreach($sql->result() as $user_name)
+  {
+    $user_name->name;
+  }
+
+?>
+  <!-- Custom styles for this template -->
+
+  <link href="<?php echo base_url('assets/css/simple-sidebar.css'); ?>" rel="stylesheet">
+  <!--navigation-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top"><!-- sticky top can be replaced with fixed-botto or fixed-top -->
+    <button class="btn btn-primary" id="menu-toggle">Menu</button>
+    <a href="#" class="navbar-brand mx-auto">Logo</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+      <ul class="navbar-nav my-2 my-lg-0">
+        <li class="nav-item active"><a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+        <li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a></li>
+        <li class="nav-item dropdown mr-4">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            <?php echo $user_name->name; ?>
+            
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="<?php echo site_url(); ?>Super_admin/my_profile"><i class="fa fa-user mr-1"></i>My Profile</a>
+              <a href="<?php echo base_url(); ?>home/logout" class="dropdown-item" id="logoutbtn_id"><i class="fa fa-power-off mr-1"></i>Logout</a>
+            </div>
+          </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+  <!--/navigation-->
+  <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">
+        Hi <?php echo $user_name->name; ?>
+      </div>
+      <div class="list-group list-group-flush">
+        <a href="<?php echo site_url(); ?>Super_admin/" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+        <a href="<?php echo site_url(); ?>Super_admin/calendar" class="list-group-item list-group-item-action bg-light">Calendar</a>
+        <div class="list-group-item  bg-light sub-menu"><a href='#drop-down' class="drop-down-n w-100">Add Users<div class='angle fas fa-angle-down right'></div></a></div>
+          <ul class="drop-down-ul">
+            <a href='<?php echo site_url(); ?>Super_admin/verify_student' class="drop-down-a"><li class="drop-down-li">Verify Student</li></a>
+            <a href='<?php echo site_url(); ?>Super_admin/add_hod' class="drop-down-a"><li class="drop-down-li">HOD</li></a>
+            <a href='<?php echo site_url(); ?>Super_admin/add_professor' class="drop-down-a"><li class="drop-down-li">Professor</li></a>
+            <a href='<?php echo site_url(); ?>Super_admin/add_lab_assistant' class="drop-down-a"><li class="drop-down-li">Lab Assistant</li></a>
+            <a href='<?php echo site_url(); ?>Super_admin/add_librarian' class="drop-down-a"><li class="drop-down-li">Librarian</li></a>
+
+          </ul>
+ 
+        <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+        
+        
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+    <script>
+$('#logoutbtn_id').on('click',function(e)
+{
+    e.preventDefault();
+    const href=$(this).attr('href')
+
+    Swal.fire({
+  title: 'ARE YOU SURE?',
+  text: "Do you want to Log Out",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#dc3545',
+  cancelButtonColor: '#6c757d',
+  confirmButtonText: 'Log Out'
+}).then((result) => {
+  if (result.value) {
+    document.location.href=href;
+  }
+})
+})
+</script>
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <div class="container-fluid">
---
+++ application/views/principal/verify_student.php
@@ -0,0 +1,140 @@
+
+
+<div class="container p-lg-4 ">
+<?php 
+if($this->session->flashdata('insert_success')){
+ echo '
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> Student sucessfully verified.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>';
+}
+if($this->session->flashdata('insert_failed')){
+    echo '
+   <div class="alert alert-danger alert-dismissible fade show" role="alert">
+     <strong>Failed!</strong> Something Wend wrong, please try again.
+     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+       <span aria-hidden="true">&times;</span>
+     </button>
+   </div>';
+   }
+?>
+    <nav aria-label="breadcrumb mt-sm-5">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item"><a href="#">Add User</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Verify Student</li>
+    </ol>
+    </nav>
+    
+
+
+<link href="<?php echo base_url('assets/bootstrap-table/bootstrap-table.min.css'); ?>" rel="stylesheet">
+
+<script src="<?php echo base_url('assets/bootstrap-table/tableExport.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-table/jspdf.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-table/jspdf.plugin.autotable.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-table/bootstrap-table.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-table/bootstrap-table-export.min.js'); ?>"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+    
+
+    <div class="container" class="col-sm-12">
+
+		<div id="toolbar">
+			<select class="form-control">
+				<option value="all">Export All</option>
+				<option value="selected">Export Selected</option>
+			</select>
+		</div>
+
+<table id="table"
+  data-show-export="true"
+  data-toolbar="#toolbar"
+  data-search="true"
+  data-sortable="true"
+  data-show-columns="true"
+  data-toggle="table" 
+  data-pagination="true"
+  class="table"
+  data-visible-search="true"
+  style="white-space: nowrap;"
+  >
+  <thead class="table-primary">
+		<tr>
+			<th data-field="state" data-checkbox="true"></th>
+			<th data-field="name" data-sortable="true">Name</th>
+			<th data-field="email" data-sortable="true">email</th>
+			<th data-field="phone" data-sortable="true">Phone</th>
+			<th data-field="dob" data-sortable="true">Dob</th>
+      <th data-field="address" data-sortable="true">Address</th>
+			<th data-field="gender" data-sortable="true">Gender</th>
+            <th data-field="Department" data-sortable="true">Department</th>
+      <th data-field="edit">Action</th>
+		</tr>
+  </thead>
+	<tbody>
+	<?php 
+	$sql=$this->db->select('*')->from('student_data')->where('s_status',1)->join('users','users.email=student_data.email')->get();
+	foreach($sql->result() as $student)
+	{
+    ?>
+		<tr>
+			<td class="bs-checkbox"><input data-index="<?php echo $student->student_id ?>" name="btSelectItem" type="checkbox"></td>
+			<td><?php echo $student->name ?></td>
+			<td><?php echo $student->email ?></td>
+			<td><?php echo $student->phone ?></td>
+      <td><?php echo $student->dob ?></td>
+      <td><?php echo $student->address ?></td>
+      <td><?php echo $student->gender ?></td>
+      <td><?php echo $student->dept ?></td>
+      <td class="text-center p-0" >
+        <a href="<?php echo base_url(); ?>Super_admin/verify_student_success/<?php echo $student->student_id; ?>"><i class="fa fa-user-check text-success mr-3"></i></a>
+        <a href="<?php echo base_url(); ?>Super_admin/verify_student_reject/<?php echo $student->student_id; ?>"><i class="fa fa-user-times text-danger"></i></a>
+      </td>
+	  	</tr>
+	<?php		
+	}
+	?>
+	</tbody>
+</table>
+
+</div>
+
+
+<script>
+  
+var $table = $('#table')
+
+  $(function() {
+    $('#toolbar').find('select').change(function () {
+      $table.bootstrapTable('destroy').bootstrapTable({
+        exportDataType: $(this).val(),
+        exportTypes: ['json', 'xml', 'csv', 'txt', 'sql', 'excel', 'pdf'],
+        columns: [
+          {
+            field: 'state',
+            checkbox: true,
+            visible: $(this).val() === 'selected'
+          }
+        ]
+      })
+    }).trigger('change')
+  })
+  
+</script>
+
+
+
+
+
+
+</div>
+
+
+
+
