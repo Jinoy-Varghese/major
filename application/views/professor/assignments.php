8a06355d7c6d9b9cd8a40531987932e279d1e277
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-03-31 14:05:34 +0530

    test

--- application/views/professor/assignments.php
+++ application/views/professor/assignments.php
@@ -182,7 +182,6 @@ if($this->session->flashdata('insert_failed')){
                 </select>
 
             </div>
-            <span></span>
 
             <div class="col-md-4 mb-3">
                 <label for="validationCustom05">Semester</label>
