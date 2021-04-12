d6bea86076626f0563eeba122112d4d50d2790d5
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-04-12 13:53:55 +0530

    Update create_question.php

--- application/views/professor/create_question.php
+++ application/views/professor/create_question.php
@@ -6,13 +6,17 @@
 <div class="container p-lg-4 ">
 <form class=" mt-5" method="get" action="<?php echo base_url();?>Professor/create_question_process">
 
-    <div class="form-row mt-5">
 
 
 
- 
-        
-        <div class="mt-2 h5"> 1. </div>
+<?php
+for($i=1;$i<=$_GET['question_no'];$i++)
+{
+
+?>
+     <div class="form-row mt-5">
+
+        <div class="mt-2 h5"> <?php echo $i ?>. </div>
         <div class="col-11">
             <input required type="text" class="col-12 form-control" placeholder="Enter your question">
         </div>
@@ -21,7 +25,7 @@
         <div class="col-md-5 ml-4 mt-3"><input required type="text" name="optionc1" class="form-control" placeholder="Option 3"></div>
         <div class="col-md-5 ml-4 mt-3"><input required type="text" name="optiond1" class="form-control" placeholder="Option 4"></div>
 
-        <div class="mt-5 h5 ml-md-4"> Choose the correct answer : </div>
+        <div class="mt-5 h5 ml-md-4 mb-md-5"> Choose the correct answer : </div>
         <div class="col-md-3 mt-md-5">
             <select required type="text" class="col-12 form-control" style="height:35px;">
                 <option disabled selected>select</option>
@@ -31,7 +35,20 @@
                 <option value="4">Option 4</option>
             </select>
         </div>
-    </div>
+        </div>
+
+        <?php
+}
+?>
+
+  
+
+
+
+
+
+
+
     <div class="form-row mt-4">
         <input class="btn btn-primary ml-1" type="submit" name="u_reg" value="Next">
     </div>
