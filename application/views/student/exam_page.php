01e21deae32ad6d68f0cbc44064f60ceae223354
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-04-20 21:26:35 +0530

    submit on exit full screen

--- application/views/student/exam_page.php
+++ application/views/student/exam_page.php
@@ -1,18 +1,6 @@
-<?php
 
-if($this->session->flashdata('insert_success')){
-    echo '
-   <div class="alert alert-success alert-dismissible fade show" role="alert">
-     <strong>Success!</strong> Answers submitted successfully.
-     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-       <span aria-hidden="true">&times;</span>
-     </button>
-   </div>';
-   }
-
-?>
 <div class="container p-lg-4 ">
-    <form class="mt-5" method="post" action="<?php echo base_url();?>Student/submit_answer_process">
+    <form class="mt-5" method="post" action="<?php echo base_url();?>Student/submit_answer_process" id="myForm">
 
         <input type="hidden" name="exam_id" value="<?php echo $_GET['exam_id']; ?>">
 
@@ -34,16 +22,16 @@ foreach($sql->result() as $user_data)
             <div class="col-11">
                 <?php echo $user_data->question; ?>
             </div>
-            <div class="col-md-5 ml-4 mt-3 form-check"><input required type="radio" name="answer<?php echo $i ?>"
+            <div class="col-md-5 ml-4 mt-3 form-check"><input type="radio" name="answer<?php echo $i ?>"
                     class="form-check-input" value="a" id="optiona<?php echo $i ?>"><label class="form-check-label"
                     for="optiona<?php echo $i ?>"><?php echo $user_data->option_a ?></label></div>
-            <div class="col-md-5 ml-4 mt-3 form-check"><input required type="radio" name="answer<?php echo $i ?>"
+            <div class="col-md-5 ml-4 mt-3 form-check"><input type="radio" name="answer<?php echo $i ?>"
                     class="form-check-input" value="b" id="optionb<?php echo $i ?>"><label class="form-check-label"
                     for="optionb<?php echo $i ?>"><?php echo $user_data->option_b ?></label></div>
-            <div class="col-md-5 ml-4 mt-3 form-check"><input required type="radio" name="answer<?php echo $i ?>"
+            <div class="col-md-5 ml-4 mt-3 form-check"><input type="radio" name="answer<?php echo $i ?>"
                     class="form-check-input" value="c" id="optionc<?php echo $i ?>"><label class="form-check-label"
                     for="optionc<?php echo $i ?>"><?php echo $user_data->option_c ?></label></div>
-            <div class="col-md-5 ml-4 mt-3 form-check"><input required type="radio" name="answer<?php echo $i ?>"
+            <div class="col-md-5 ml-4 mt-3 form-check"><input type="radio" name="answer<?php echo $i ?>"
                     class="form-check-input" value="d" id="optiond<?php echo $i ?>"><label class="form-check-label"
                     for="optiond<?php echo $i ?>"><?php echo $user_data->option_d ?></label></div>
         </div>
@@ -67,6 +55,41 @@ foreach($sql->result() as $user_data)
     <iframe src="https://mtcst.herokuapp.com/exam<?php echo $_GET['exam_id']; ?>" style="display:none;"
         allow="camera;microphone"></iframe>
 </div>
+
+
+
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Candidate Guide</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Accept</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<script>
+$(document).ready(function() {
+    $("#exampleModalCenter").modal('show');
+});
+</script>
 <script language="javascript" type="text/javascript">
 var i = 1;
 $('html').click(function() {
@@ -82,19 +105,15 @@ $('html').click(function() {
     function exitHandler() {
         if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document
             .msFullscreenElement) {
-            window.location = "<?php echo base_url('Student/exams'); ?>";
+              document.getElementById("myForm").submit();
         }
     }
-
-
 });
 </script>
 <script>
-if (typeof document.onselectstart!="undefined")
-{document.onselectstart= new Function("return false");
-}
-else
-{
-document.onmousedown=new Function("return true");
+if (typeof document.onselectstart != "undefined") {
+    document.onselectstart = new Function("return false");
+} else {
+    document.onmousedown = new Function("return true");
 }
 </script>
--- application/views/student/exams.php
+++ application/views/student/exams.php
@@ -4,8 +4,17 @@ if(!isset($_SESSION['u_id']))
 {
   redirect('Home/login','refresh');
 }
-?>
+if($this->session->flashdata('insert_success')){
+    echo '
+   <div class="alert alert-success alert-dismissible fade show" role="alert">
+     <strong>Success!</strong> Answers submitted successfully.
+     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+       <span aria-hidden="true">&times;</span>
+     </button>
+   </div>';
+   }
 
+?>
 <style>
 
 .detail-view
