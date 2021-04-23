3fbe14007603471b04422200786a5e53d77925f5
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-04-23 11:30:54 +0530

    test_finished

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # major
  college_management system for
 MarThoma College of Science and Technology
+
+
+Note : Remove the comment on line 330 in host\application\views\student\exam_page.php page if you are hosting this website on ssl certified domain.(Since video streaming are not supported on non ssl certified host)
--- application/views/student/exam_page.php
+++ application/views/student/exam_page.php
@@ -234,7 +234,7 @@ foreach($sql->result() as $user_data)
             if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !
                 document
                 .msFullscreenElement) {
-               //document.getElementById("myForm").submit();
+               document.getElementById("myForm").submit();
             }
         }
     });
@@ -327,7 +327,9 @@ $time=seconds_from_time($time);
         return (n < 10 ? '0' : '') + n;
     }
     </script>
- <!--   <script>
+ <!--   remove this comment if this site is hosting in a ssl certified domain.
+     
+     <script>
     var video = document.querySelector("#videoElement");
 
     if (navigator.mediaDevices.getUserMedia) {
