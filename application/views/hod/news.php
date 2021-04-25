032faf4f347b150b771b01c374b1a2d41b2328b8
Arun-Ayyappan <77910661+Arun-Ayyappan@users.noreply.github.com>
2021-04-25 12:43:29 +0530

    Update news.php

--- application/views/hod/news.php
+++ application/views/hod/news.php
@@ -32,15 +32,42 @@ if($this->session->flashdata('insert_failed')){
                                                             <div class="col-md-4 mb-3">
                                                                                 <label
                                                                                                     for="validationCustom04">News</label>
-                                                                                <input type='text'
-                                                                                                    class="form-control"
+                                                                                <input type='text' class="form-control"
                                                                                                     id="validationCustom01"
                                                                                                     value="" name="news"
                                                                                                     required>
-                                                                                <div class="mt-3">
+                                                            </div>                                        
+                                                                                <div class="col-md-4 mb-3">
+                                                                                                    <label
+                                                                                                                        for="validationCustom03">File</label>
+                                                                                                    <div
+                                                                                                                        class="custom-file">
+                                                                                                                        <label for="note_file_id"
+                                                                                                                                            class="form-control border-primary"
+                                                                                                                                            style="height:35px;"><i
+                                                                                                                                                                class="fas fa-camera"></i>&nbsp&nbspChoose
+                                                                                                                                            File</label>
+                                                                                                                        <input type="file" class="custom-file-input"
+                                                                                                                                            name="note_file"
+                                                                                                                                            id="note_file_id"
+                                                                                                                                            required
+                                                                                                                                            style="display:none;">
+                                                                                                                        <div
+                                                                                                                                            class="invalid-feedback">
+                                                                                                                                            Please
+                                                                                                                                            provide
+                                                                                                                                            the
+                                                                                                                                            notes.
+                                                                                                                        </div>
+                                                                                                    </div>
+                                                                                </div>
+
+                                                                                
+                                                                                <div class="col-md-7 mb-3">
                                                                                                     <input class="btn btn-primary"
                                                                                                                         type="submit"
                                                                                                                         name="u_reg"
                                                                                                                         value="Submit">
-                                                                                </div>
+                                                                                </div>      
+                                        </div>
                     </form>
