79bcd958fae94390e95744ca503a7885433bd855
Arun-Ayyappan <77910661+Arun-Ayyappan@users.noreply.github.com>
2021-04-28 07:45:32 +0530

    Update offline.php

---
+++ application/views/professor/offline.php
@@ -0,0 +1,78 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+if(!isset($_SESSION['u_id']))
+{
+  redirect('Home/login','refresh');
+}
+?>
+<link href="<?php echo base_url('assets/bootstrap-table/bootstrap-table.min.css'); ?>" rel="stylesheet">
+
+<script src="<?php echo base_url('assets/bootstrap-table/tableExport.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-table/jspdf.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-table/jspdf.plugin.autotable.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-table/bootstrap-table.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap-table/bootstrap-table-export.min.js'); ?>"></script>
+
+
+<div class="container p-lg-4 ">
+    <?php 
+if($this->session->flashdata('registered_success')){
+ echo '
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> New Complaint Registered.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>';
+}
+if($this->session->flashdata('registration_failed')){
+    echo '
+   <div class="alert alert-danger alert-dismissible fade show" role="alert">
+     <strong>Failed!</strong> Something Wend wrong, please try again.
+     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+       <span aria-hidden="true">&times;</span>
+     </button>
+   </div>';
+   }
+?>
+ <nav aria-label="breadcrumb mt-sm-5">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Offline Mark</a></li>
+    </nav>
+    <form class="needs-validation mt-5" novalidate method="post"
+        action="<?php echo base_url();?>Professor/lab_complaint_data">
+        <div class="form-row">
+     
+            <div class="col-md-6 col-6 mb-3">
+                <label for="validationCustom01">Student Name</label>
+                <input type="text" class="form-control" id="validationCustom01" name="s_name" required>
+                <div class="invalid-feedback">
+                    Please provide student name
+                </div>
+            </div>
+
+            <div class="col-md-6 col-6 mb-3">
+                <label for="validationCustom01">Subject</label>
+                <input type="text" class="form-control" id="validationCustom01" name="subject" required>
+                <div class="invalid-feedback">
+                    Please provide a subject
+                </div>
+            </div>
+            <div class="col-md-6 col-6 mb-3">
+                <label for="validationCustom01">Semester</label>
+                <input type="text" class="form-control" id="validationCustom01" name="sem" required>
+                <div class="invalid-feedback">
+                    Please provide semester
+                </div>
+            </div>
+            <div class="col-md-6 col-6 mb-3">
+                <label for="validationCustom01">Mark</label>
+                <input type="text" class="form-control" id="validationCustom01" name="mark" required>
+                <div class="invalid-feedback">
+                    Please provide mark
+                </div>
+            </div>
+            </div>
+            <input class="btn btn-primary" type="submit" name="complaint_reg" value="Submit">
+    </form>
