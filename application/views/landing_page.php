92f92f4ff52937701df3994fe18c9249217cca4c
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-05-08 15:49:27 +0530

    --- Auto-Git Commit ---

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -1,18 +1,66 @@
-<link rel="shortcut icon" type="image/png" href="<?php echo base_url('assets/image/marthoma.png'); ?>"/>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html amp lang="en">
 
 
-<div style="display:flex;justify-content:center;align-items:center;height:98vh;">
-    <a href="<?php echo base_url(); ?>Home/login">Login</a>
-</div>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" type="image/png" href="<?php echo base_url('assets/image/marthoma.png'); ?>"/>
+    <script type="application/ld+json">
+    {
 
-<style>
-a:hover
-{
-    background:#7290aa;
-    color:white;
-}
-a
-{
-    text-decoration:none;color:black;border:1px solid black;width:220px;height:50px;border-radius:15px;align-items:center;display:flex;justify-content:center;
-}
-</style>
+        "image": [
+            "logo.jpg"
+        ]
+    }
+    </script>
+    <title>MTCST</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/fontawesome-5.11.2/css/all.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap-4.3.1/css/bootstrap.min.css'); ?>">
+</head>
+
+<body>
+
+
+
+    <!-- We will put our React component inside this div. -->
+    <div id="header"></div>
+
+    <!-- Load React. -->
+    <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+    <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+    <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+    <!-- Load our React component. -->
+    <script type="text/babel">
+    'use strict';
+        class Welcome extends React.Component {
+            constructor(props) {
+                super(props);
+                this.state = {
+ 
+
+                }
+            }
+            render(){
+                return (<div>
+
+                    </div>
+                );
+            }
+        }
+        
+        ReactDOM.render(<Welcome />, document.querySelector('#header'));
+    </script>
+
+</body>
+
+</html>
