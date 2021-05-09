630fcfa3dc83b7f51ff229b9a89036d745aa1040
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-05-09 12:18:51 +0530

    --- Auto-Git Commit ---

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -52,53 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Logo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav  ">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url('Home/login'); ?>">Login</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
-
-
-    <div class="col-12 p-0 m-0">
-        <amp-img height="85" width="200" class="col-12 p-0 m-0" id="lap_mtcst" src="<?php echo base_url('assets/image/mtcst_cloud.jpeg') ?>"
-            alt="marthoma college of science and technology ayur" layout="responsive"></amp-img>
-        <amp-img height="155" width="100" class="col-12 p-0 m-0" id="mobile_mtcst"
-            src="<?php echo base_url('assets/image/mtcst_cloud_mobile.jpeg') ?>"
-            alt="marthoma college of science and technology ayur" layout="responsive"></amp-img>
-
-    </div>
 
 
 
@@ -117,10 +70,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div id="header"></div>
     <!-- Load React. -->
     <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
-    <script async src="<?php echo base_url('assets/bootstrap-4.3.1/js/bootstrap.min.js'); ?>"></script>
-    <script async src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
-    <script async src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
-    <script async src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+    <script src="<?php echo base_url('assets/bootstrap-4.3.1/js/bootstrap.min.js'); ?>"></script>
+    <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+    <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
 
 
     <!-- Load our React component. -->
@@ -137,6 +90,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             render(){
                 return (<div>
 
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Logo</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul className="navbar-nav  ">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="<?php echo base_url('Home/login'); ?>">Login</a>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+
+    <div className="col-12 p-0 m-0">
+        <amp-img height="85" width="200" className="col-12 p-0 m-0" id="lap_mtcst" src="<?php echo base_url('assets/image/mtcst_cloud.jpeg') ?>"
+            alt="marthoma college of science and technology ayur" layout="responsive"></amp-img>
+        <amp-img height="155" width="100" className="col-12 p-0 m-0" id="mobile_mtcst"
+            src="<?php echo base_url('assets/image/mtcst_cloud_mobile.jpeg') ?>"
+            alt="marthoma college of science and technology ayur" layout="responsive"></amp-img>
+
+    </div>
                     </div>
                 );
             }
