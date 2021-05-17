b2ae2597c4561268ed65a745035b97ae14424fc1
Deva-Dathan <78137542+Deva-Dathan@users.noreply.github.com>
2021-05-17 21:00:24 +0530

    udpate

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -130,7 +130,7 @@ public function upload_assignment($as_id)
    $assign_upload=array('assign_id'=>$as_id,'assign_by'=>$_SESSION['u_id'],'assign_sem'=>$assign_sem,'assign_subject'=>$subject,'assign_to'=>$tr_id,'assign_topic'=>$assign_topic,'assign_date'=>date('Y-m-d'),'assign_file'=>$target);
    $this->db->insert('assignment_submit',$assign_upload);
    $this->session->set_flashdata('add_assign',"Successfully Uploaded");
-   //redirect('Student/assignment','refresh');
+   redirect('Student/assignment','refresh');
 }
 
 public function view_subject_ajax()
