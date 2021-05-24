a3445a83f94020040e39f9c3b9ff4cbe9588eeae
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-05-24 09:45:53 +0530

    Update assign_teachers.php

--- application/views/hod/assign_teachers.php
+++ application/views/hod/assign_teachers.php
@@ -1,6 +1,6 @@
 <div class="container p-lg-4 ">
 
-<?php 
+    <?php 
 if($this->session->flashdata('insert_success')){
  echo '
 <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -20,45 +20,44 @@ if($this->session->flashdata('insert_failed')){
    </div>';
    }
 ?>
-<style>
-  .custom-button
-  {
-    color:#007BFF !important;
-    cursor:pointer;
-  }
-  .custom-button:hover
-  {
-    color:white !important;
-    background:#007BFF;
-    cursor:pointer;
-  }
-</style>
+    <style>
+    .custom-button {
+        color: #007BFF !important;
+        cursor: pointer;
+    }
+
+    .custom-button:hover {
+        color: white !important;
+        background: #007BFF;
+        cursor: pointer;
+    }
+    </style>
     <nav aria-label="breadcrumb mt-sm-5">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item">Assign Teacher</li>
-    </ol>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item">Assign Teacher</li>
+        </ol>
     </nav>
+    <div class="row mt-5">
+    <div class="col-md-8"></div>
+        <div class="col-md-4">
+            <a href="#" class="btn border-primary col-12 custom-button pl-0 pr-0">Request teacher from other department</a>
+        </div>
+    </div>
 
-<div class="row col-12 justify-content-end mt-5">
-  <div class="col-md-4 p-0 ">
-  <a href="" class="btn border-primary col-12 custom-button float-right">Request teacher from other department</a>
-  </div>
-</div>
+    <form class="needs-validation mt-3" novalidate method="post"
+        action="<?php echo base_url();?>hod/assign_teacher_process">
+
+        <div class="form-row mt-5">
 
 
-<form class="needs-validation mt-3" novalidate method="post" action="<?php echo base_url();?>hod/assign_teacher_process">
 
-<div class="form-row mt-5">
-            
-            
-
-                <div class="col-md-4 mb-3">
+            <div class="col-md-4 mb-3">
                 <label for="validationCustom06">Course</label>
 
-            <select class="custom-select" id="validationCustom08" required name="course">
-            <option selected disabled value="">Choose...</option>
-            <?php 
+                <select class="custom-select" id="validationCustom08" required name="course">
+                    <option selected disabled value="">Choose...</option>
+                    <?php 
 
             $id=$_SESSION['u_id'];
             $this->db->select('*');
@@ -82,8 +81,8 @@ if($this->session->flashdata('insert_failed')){
             echo "<option value='$course_data->course_name'>$course_data->course_name</option>";
             }
             ?>
-            </select>
-            <div class="valid-feedback">
+                </select>
+                <div class="valid-feedback">
                     Looks good!
                 </div>
                 <div class="invalid-feedback">
@@ -93,11 +92,11 @@ if($this->session->flashdata('insert_failed')){
 
 
             <div class="col-md-4 mb-3">
-             <label for="validationCustom06">Subject</label>
+                <label for="validationCustom06">Subject</label>
 
-            <select class="custom-select" id="subject" required name="subject">
-            <option selected disabled value="select">Choose...</option>
-            <?php 
+                <select class="custom-select" id="subject" required name="subject">
+                    <option selected disabled value="select">Choose...</option>
+                    <?php 
 
             $id=$_SESSION['u_id'];
             $this->db->select('*');
@@ -119,19 +118,19 @@ if($this->session->flashdata('insert_failed')){
             echo "<option value='$user_data->sub_name'>$user_data->sub_name</option>";
             }
             ?>
-            </select>
-            <div class="valid-feedback">
+                </select>
+                <div class="valid-feedback">
                     Looks good!
                 </div>
                 <div class="invalid-feedback">
                     Please enter a Subject.
                 </div>
             </div>
-            
-                <div class="col-md-4 mb-3">
+
+            <div class="col-md-4 mb-3">
                 <label for="validationCustom05">Semester</label>
-       
-                    <select class="custom-select" id="semester" required name="semester">
+
+                <select class="custom-select" id="semester" required name="semester">
                     <option selected disabled value="">Choose...</option>
 
 
@@ -139,8 +138,8 @@ if($this->session->flashdata('insert_failed')){
 
 
 
-                    </select>
-                    <div class="valid-feedback">
+                </select>
+                <div class="valid-feedback">
                     Looks good!
                 </div>
                 <div class="invalid-feedback">
@@ -148,11 +147,11 @@ if($this->session->flashdata('insert_failed')){
                 </div>
             </div>
             <div class="col-md-4 mb-3">
-             <label for="validationCustom06">Teacher</label>
+                <label for="validationCustom06">Teacher</label>
 
-            <select class="custom-select" id="validationCustom06" required name="teacher">
-            <option selected disabled value="">Choose...</option>
-            <?php 
+                <select class="custom-select" id="validationCustom06" required name="teacher">
+                    <option selected disabled value="">Choose...</option>
+                    <?php 
 
             $id=$_SESSION['u_id'];
             $this->db->select('*');
@@ -177,8 +176,8 @@ if($this->session->flashdata('insert_failed')){
             echo "<option value='$user_data->email'>$user_data->name</option>";
             }
             ?>
-            </select>
-            <div class="valid-feedback">
+                </select>
+                <div class="valid-feedback">
                     Looks good!
                 </div>
                 <div class="invalid-feedback">
@@ -186,64 +185,68 @@ if($this->session->flashdata('insert_failed')){
                 </div>
             </div>
 
-           
 
-            
-            </div>
-            <div class="form-row">
+
+
+        </div>
+        <div class="form-row">
             <input class="btn btn-primary ml-1" type="submit" name="u_reg" value="Assign Subject">
-            </div>
+        </div>
 
     </form>
 </div>
 <script type="text/javascript">
-$(document).ready(function(){
+$(document).ready(function() {
 
-$("#subject").change(function(){
-    var subject = $(this).val();
+    $("#subject").change(function() {
+        var subject = $(this).val();
 
-    $.ajax({
-        url: 'http://localhost/major/hod/view_semester_ajax',
-        type: 'post',
-        data: {post_subject:subject},
-        dataType: 'json',
-        success:function(response){
-          
-            var len = response.length;
-            
-            $("#semester").empty();
-            $("#semester").append("<option disabled value='select' selected>--Select--</option>");
-            for( var i = 0; i<len; i++){
-                var sem = response[i]['sem'];
-                
-                $("#semester").append("<option value='s"+sem+"'>"+sem+"</option>");
+        $.ajax({
+            url: 'http://localhost/major/hod/view_semester_ajax',
+            type: 'post',
+            data: {
+                post_subject: subject
+            },
+            dataType: 'json',
+            success: function(response) {
 
+                var len = response.length;
+
+                $("#semester").empty();
+                $("#semester").append(
+                    "<option disabled value='select' selected>--Select--</option>");
+                for (var i = 0; i < len; i++) {
+                    var sem = response[i]['sem'];
+
+                    $("#semester").append("<option value='s" + sem + "'>" + sem +
+                        "</option>");
+
+                }
             }
-        }
+        });
     });
-});
 
 });
 </script>
 
 
-    <script>
+<script>
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
 })();
 </script>
