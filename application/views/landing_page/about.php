32eab279848111972d597825c8413fdcf878f978
Deva-Dathan <78137542+Deva-Dathan@users.noreply.github.com>
2021-07-11 22:25:33 +0530

    update

--- application/views/landing_page/about.php
+++ application/views/landing_page/about.php
@@ -3,49 +3,46 @@ defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
 <div class="col-md-12">
-
-<h1>ABOUT US</h1>
+<h1 class="font-weight-bold text-center mt-2">ABOUT US</h1>
+</div>
+<hr>
 
 <div class="container">
 <div class="row">
 <div class="col-12 col-md-6 ml-n2">
-<img src="<?php echo base_url('assets/img/found.jfif');?>" height=400px width=500px>
+<img src="<?php echo base_url('assets/img/found.jfif');?>">
 </div>
 
-<div class="col-12 col-md-6">
-<img src="<?php echo base_url('assets/img/founder.jfif')?>" height=400px width=500px>
+<div class="col-12 col-md-6 text-center">
+<img src="<?php echo base_url('assets/img/founder.jfif')?>">
 </div>
 </div>
 </div>
+<hr>
 
-</div>
 
-<div class="conatainer mt-5">
-<div class="col-12 text-justify pl-5 pr-5" style="font-size:18px; line-height:40px;">
+
+<div class="col-12 text-justify pl-5 pr-5" style="font-size:18px; line-height:30px;">
 The college started functioning in September 13, 1995 as a self financing institution with 20 students each on the rolls in the undergraduate courses of B.Sc. Polymer Chemistry, B.Sc. Electronics and B.Sc. Computer Science.
 The first batch of “Mar Thoma College graduates” rolled out in 1998, what we call as the ‘academic baptism’ of the college. In 2001, a new Career-related course titled B.Sc. Botany and Biotechnology was introduced. B.Com. with Computer Applications was added to the list in 2005.
 The National Commission for Minority Educational Institutions, Govt. of India vide its order F.No. 799 of 2009/38049 granted Minority Status Certification to the college on September 24, 2009. In the academic year 2013-14, B.B.A. and B.A. English Language and Literature courses were also started.
 In addition to these, five PG Programs were sanctioned by the University from 2005 to 2020, elevating its status to that of a Post graduate College. MA English course started in  October, 2020 .
 Thus in a short span of time the college has developed into a sprawling campus with state of the art amenities. Now the college has, seven under-graduate and six post-graduate courses with due affiliation from the University of Kerala.
-
- This transformation is truly remarkable and astonishing thanks to His abundant mercy and blessings and the visionary excellence of His Grace The Most Rev. Dr. Joseph Mar Thoma Metropolitan, our founding father. The college is undoubtedly the fruit of the unbending resolve, intrepid bravery and venturesome boldness of His Grace the Metropolitan. While celebrating our twentieth year we proudly salute the wisdom, vision and passion of our founding father. With a deep sense of gratitude we remember all those who worked in various capacities and contributed to building the institution to this level. We strongly believe that a literate and educated citizenry is vital to a democratic society and all people are entitled to a high quality education, grounded in sensitivity to individual dignity and professional integrity. 
+This transformation is truly remarkable and astonishing thanks to His abundant mercy and blessings and the visionary excellence of His Grace The Most Rev. Dr. Joseph Mar Thoma Metropolitan, our founding father. The college is undoubtedly the fruit of the unbending resolve, intrepid bravery and venturesome boldness of His Grace the Metropolitan. While celebrating our twentieth year we proudly salute the wisdom, vision and passion of our founding father. With a deep sense of gratitude we remember all those who worked in various capacities and contributed to building the institution to this level. We strongly believe that a literate and educated citizenry is vital to a democratic society and all people are entitled to a high quality education, grounded in sensitivity to individual dignity and professional integrity. 
 Mar Thoma College of Science & Technology is owned and managed by a Registered Society namely The Mar Thoma Educational, Technical, Training and Research Centre (Mar Thoma ETTARC), under the Trivandrum-Quilon Diocese of the Mar Thoma Church.
 This College is affiliated to the University of Kerala and has 8 undergraduate and 6 post graduate courses. 
 </div>
-</div>
 <hr>
 
-<div class="container ml-n1">
-<div class="col-12">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3941.9138402083263!2d76.8656377146063!3d8.887602693619046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05de60959ae3ff%3A0x3dcccdd22786e596!2sMar%20Thoma%20College%20Of%20Science%20%26%20Technology!5e0!3m2!1sen!2sin!4v1626006601766!5m2!1sen!2sin" width="1500" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<div class="col-md-12">
+<iframe class="col-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3941.9138402083263!2d76.8656377146063!3d8.887602693619046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05de60959ae3ff%3A0x3dcccdd22786e596!2sMar%20Thoma%20College%20Of%20Science%20%26%20Technology!5e0!3m2!1sen!2sin!4v1626006601766!5m2!1sen!2sin" width="1500" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 </div>
-</div><br>
+<br>
+<hr>
 
+<div class="mb-5 ml-5 font-weight-bold">
 
-
-<div class="container ml-n5">
-<div class="col-12 font-weight-bold" style="padding-left:100px;">
-<h4><u>CONTACT US</u></h4>
+<h4 class="font-weight-bold"><u>CONTACT US</u></h4>
 <div style="color:red;">PRINCIPAL</div>
 Prof. Dr. Joseph Mathai<br> 
 Mar Thoma College of Science and Technology<br> 
@@ -54,5 +51,4 @@ Kollam (Dist.),Kerala<br>
 Ph:0474-2475444; 0474-2475074;9387255515<br> 
 email: josamkar@gmail.com<br>
 ayurcollege1995@gmail.com<br>
-</div>
 </div>
