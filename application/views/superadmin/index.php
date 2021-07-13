3626d04b92c103c5e8d99690bd9b8169adb622e1
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-07-13 07:18:19 +0530

    --- Auto-Git Commit ---

--- application/views/superadmin/index.php
+++ application/views/superadmin/index.php
@@ -6,113 +6,47 @@
         <script>
             am4core.ready(function() {
 
-            // Themes begin
-            am4core.useTheme(am4themes_animated);
-            // Themes end
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
 
-            var chart = am4core.create("chartdiv", am4charts.XYChart);
-            chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+var chart = am4core.create("chartdiv", am4charts.XYChart);
 
-            chart.data = [
-            {
-                country: "USA",
-                visits: 725
-            },
-            {
-                country: "China",
-                visits: 882
-            },
-            {
-                country: "Japan",
-                visits: 809
-            },
-            {
-                country: "Germany",
-                visits: 322
-            },
-            {
-                country: "UK",
-                visits: 122
-            },
-            {
-                country: "France",
-                visits: 114
-            },
-            {
-                country: "India",
-                visits: 984
-            },
-            {
-                country: "Spain",
-                visits: 711
-            },
-            {
-                country: "Netherlands",
-                visits: 465
-            },
+var data = [];
+var value = 50;
+for(var i = 0; i < 300; i++){
+  var date = new Date();
+  date.setHours(0,0,0,0);
+  date.setDate(i);
+  alert(date);
+  value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+  data.push({date:date, value: value});
+}
 
-            ];
+chart.data = data;
 
-            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-            categoryAxis.renderer.grid.template.location = 0;
-            categoryAxis.dataFields.category = "country";
-            categoryAxis.renderer.minGridDistance = 40;
-            categoryAxis.fontSize = 9;
-            
+// Create axes
+var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.minGridDistance = 60;
 
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-            valueAxis.min = 0;
-            valueAxis.max = 1000;
-            valueAxis.strictMinMax = true;
-            valueAxis.renderer.minGridDistance = 30;
-            valueAxis.renderer.labels.template.fontSize = 9;
-            // axis break
-            var axisBreak = valueAxis.axisBreaks.create();
-            axisBreak.startValue = 400;
-            axisBreak.endValue = 700;
-            //axisBreak.breakSize = 0.005;
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-            // fixed axis break
-            var d = (axisBreak.endValue - axisBreak.startValue) / (valueAxis.max - valueAxis.min);
-            axisBreak.breakSize = 0.05 * (1 - d) / d; // 0.05 means that the break will take 5% of the total value axis height
+// Create series
+var series = chart.series.push(new am4charts.LineSeries());
+series.dataFields.valueY = "value";
+series.dataFields.dateX = "date";
+series.tooltipText = "{value}"
 
-            // make break expand on hover
-            var hoverState = axisBreak.states.create("hover");
-            hoverState.properties.breakSize = 1;
-            hoverState.properties.opacity = 0.1;
-            hoverState.transitionDuration = 1500;
+series.tooltip.pointerOrientation = "vertical";
 
-            axisBreak.defaultState.transitionDuration = 1000;
-            /*
-            // this is exactly the same, but with events
-            axisBreak.events.on("over", function() {
-            axisBreak.animate(
-                [{ property: "breakSize", to: 1 }, { property: "opacity", to: 0.1 }],
-                1500,
-                am4core.ease.sinOut
-            );
-            });
-            axisBreak.events.on("out", function() {
-            axisBreak.animate(
-                [{ property: "breakSize", to: 0.005 }, { property: "opacity", to: 1 }],
-                1000,
-                am4core.ease.quadOut
-            );
-            });*/
+chart.cursor = new am4charts.XYCursor();
+chart.cursor.snapToSeries = series;
+chart.cursor.xAxis = dateAxis;
 
-            var series = chart.series.push(new am4charts.ColumnSeries());
-            series.dataFields.categoryX = "country";
-            series.dataFields.valueY = "visits";
-            series.columns.template.tooltipText = "{valueY.value}";
-            series.columns.template.tooltipY = 0;
-            series.columns.template.strokeOpacity = 0;
-            
-            // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
-            series.columns.template.adapter.add("fill", function(fill, target) {
-            return chart.colors.getIndex(target.dataItem.index);
-            });
+//chart.scrollbarY = new am4core.Scrollbar();
+chart.scrollbarX = new am4core.Scrollbar();
 
-            }); // end am4core.ready()
+}); // end am4core.ready()
             am4core.ready(function() {
 
 // Themes begin
