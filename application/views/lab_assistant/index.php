6b633be3309ef3453c5a501e2c14b0ae03160974
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-07-14 18:37:32 +0530

    Update index.php

--- application/views/lab_assistant/index.php
+++ application/views/lab_assistant/index.php
@@ -6,113 +6,45 @@
         <script>
             am4core.ready(function() {
 
-            // Themes begin
-            am4core.useTheme(am4themes_animated);
-            // Themes end
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
 
-            var chart = am4core.create("chartdiv", am4charts.XYChart);
-            chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
-            chart.data = [
-            {
-                country: "USA",
-                visits: 725
-            },
-            {
-                country: "China",
-                visits: 882
-            },
-            {
-                country: "Japan",
-                visits: 809
-            },
-            {
-                country: "Germany",
-                visits: 322
-            },
-            {
-                country: "UK",
-                visits: 122
-            },
-            {
-                country: "France",
-                visits: 114
-            },
-            {
-                country: "India",
-                visits: 984
-            },
-            {
-                country: "Spain",
-                visits: 711
-            },
-            {
-                country: "Netherlands",
-                visits: 465
-            },
 
-            ];
+// create chart
+var chart = am4core.create("chartdiv", am4charts.GaugeChart);
+chart.innerRadius = -15;
 
-            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-            categoryAxis.renderer.grid.template.location = 0;
-            categoryAxis.dataFields.category = "country";
-            categoryAxis.renderer.minGridDistance = 40;
-            categoryAxis.fontSize = 9;
-            
+var axis = chart.xAxes.push(new am4charts.ValueAxis());
+axis.min = 0;
+axis.max = 100;
+axis.strictMinMax = true;
 
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-            valueAxis.min = 0;
-            valueAxis.max = 1000;
-            valueAxis.strictMinMax = true;
-            valueAxis.renderer.minGridDistance = 30;
-            valueAxis.renderer.labels.template.fontSize = 9;
-            // axis break
-            var axisBreak = valueAxis.axisBreaks.create();
-            axisBreak.startValue = 400;
-            axisBreak.endValue = 700;
-            //axisBreak.breakSize = 0.005;
+var colorSet = new am4core.ColorSet();
 
-            // fixed axis break
-            var d = (axisBreak.endValue - axisBreak.startValue) / (valueAxis.max - valueAxis.min);
-            axisBreak.breakSize = 0.05 * (1 - d) / d; // 0.05 means that the break will take 5% of the total value axis height
+var gradient = new am4core.LinearGradient();
+gradient.stops.push({color:am4core.color("red")})
+gradient.stops.push({color:am4core.color("yellow")})
+gradient.stops.push({color:am4core.color("green")})
 
-            // make break expand on hover
-            var hoverState = axisBreak.states.create("hover");
-            hoverState.properties.breakSize = 1;
-            hoverState.properties.opacity = 0.1;
-            hoverState.transitionDuration = 1500;
+axis.renderer.line.stroke = gradient;
+axis.renderer.line.strokeWidth = 15;
+axis.renderer.line.strokeOpacity = 1;
 
-            axisBreak.defaultState.transitionDuration = 1000;
-            /*
-            // this is exactly the same, but with events
-            axisBreak.events.on("over", function() {
-            axisBreak.animate(
-                [{ property: "breakSize", to: 1 }, { property: "opacity", to: 0.1 }],
-                1500,
-                am4core.ease.sinOut
-            );
-            });
-            axisBreak.events.on("out", function() {
-            axisBreak.animate(
-                [{ property: "breakSize", to: 0.005 }, { property: "opacity", to: 1 }],
-                1000,
-                am4core.ease.quadOut
-            );
-            });*/
+axis.renderer.grid.template.disabled = true;
 
-            var series = chart.series.push(new am4charts.ColumnSeries());
-            series.dataFields.categoryX = "country";
-            series.dataFields.valueY = "visits";
-            series.columns.template.tooltipText = "{valueY.value}";
-            series.columns.template.tooltipY = 0;
-            series.columns.template.strokeOpacity = 0;
-            
-            // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
-            series.columns.template.adapter.add("fill", function(fill, target) {
-            return chart.colors.getIndex(target.dataItem.index);
-            });
+var hand = chart.hands.push(new am4charts.ClockHand());
+hand.radius = am4core.percent(97);
 
-            }); // end am4core.ready()
+setInterval(function() {
+    hand.showValue(30, 1000, am4core.ease.cubicOut);
+}, 2000);
+
+
+}); // end am4core.ready()
+
+
             am4core.ready(function() {
 
 // Themes begin
@@ -122,14 +54,24 @@ am4core.useTheme(am4themes_animated);
 var chart = am4core.create("piechart", am4charts.PieChart);
 chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
+
+<?php
+
+$sql=$this->db->get_where('lab_complaints',array('status'=>0));
+$fixed=$sql->num_rows();
+$sql=$this->db->get_where('lab_complaints',array('status'=>1));
+$not_fixed=$sql->num_rows();
+?>
+
+
 chart.data = [
   {
     country: "Fixed",
-    value: 401
+    value: <?php echo $fixed; ?>
   },
   {
     country: "Not Fixed",
-    value: 300
+    value: <?php echo $not_fixed; ?>
   }
 
 ];
