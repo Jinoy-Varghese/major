04c2f2d2b2ba39e446f0ab2958a091b67426bb92
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-07-15 11:08:13 +0530

    --- Auto-Git Commit ---

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -530,6 +530,41 @@ class Home extends CI_Controller
       redirect('Home/login','refresh');
     }
    }
+   public function mailer()
+   {
+    use PHPMailer\PHPMailer\PHPMailer; 
+    use PHPMailer\PHPMailer\Exception; 
+    use PHPMailer\PHPMailer\SMTP; 
+      
+    require 'vendor/autoload.php'; 
+      
+    $mail = new PHPMailer(true); 
+      $mail->isSMTP();
+    try { 
+       
+        $mail->SMTPDebug = 0;  
+        $mail->Mailer='smtp';                                      
+        $mail->isSMTP();                                             
+        $mail->Host       = 'mail.postale.io';                     
+        $mail->SMTPAuth   = true;                              
+        $mail->Username   = 'support@mtcst.ml';                  
+        $mail->Password   = 'oxaqguAMkgq3';                         
+        $mail->SMTPSecure = 'STARTTLS';                               
+        $mail->Port       = 587;   
+        $mail->IsHTML(TRUE);
+        $mail->setFrom('support@mtcst.ml', 'MTCST'); 
+        $maill="jinoy.v2000@gmail.com";           
+        $mail->addAddress($maill); 
+                       
+        $mail->Subject = 'Subject'; 
+        $mail->Body    = 'helloo jino.aa....  this is a message from eparish '; 
+        $mail->AltBody = 'Body in plain text for non-HTML mail clients'; 
+        $mail->send(); 
+        echo "Mail has been sent successfully!"; 
+    } catch (Exception $e) { 
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; 
+    } 
+   }
 
 
 }
