7a8db963dfdd0acc4c7e5d25cadc731baba8bf86
Jinoy Varghese <68021115+Jinoy-Varghese@users.noreply.github.com>
2021-07-15 22:38:11 +0530

    --- Auto-Git Commit ---

--- application/views/professor/subject_attendance.php
+++ application/views/professor/subject_attendance.php
@@ -1,5 +1,4 @@
 <div class="container p-lg-4 ">
-
     <?php 
 if($this->session->flashdata('insert_success')){
  echo '
@@ -20,8 +19,6 @@ if($this->session->flashdata('insert_failed')){
    </div>';
    }
 ?>
-
-
   
 <style>
     fieldset {
@@ -63,7 +60,6 @@ if($this->session->flashdata('insert_failed')){
   text-indent: -9999px;
   content: "☆";
   margin-left:-12px;
-
   color: #888;
 }
 .star-cb-group > input:checked ~ label:before, .star-cb-group > input + label:hover ~ label:before, .star-cb-group > input + label:hover:before {
@@ -71,15 +67,12 @@ if($this->session->flashdata('insert_failed')){
   color: #e52;
   text-shadow: 0 0 1px #333;
   
-
 }
 .star-cb-group > .star-cb-clear + label {
   text-indent: -9999px;
   width: 0.5em;
   margin-left: -0.5em;
     
-
-
 }
 .star-cb-group > .star-cb-clear + label:before {
   width: 0.5em;
@@ -97,18 +90,14 @@ if($this->session->flashdata('insert_failed')){
   
 }
 </style>  
-
-
     <link href="<?php echo base_url('assets/bootstrap-table/bootstrap-table.min.css'); ?>" rel="stylesheet">
     <script src="<?php echo base_url('assets/bootstrap-table/jspdf.min.js'); ?>"></script>
     <script src="<?php echo base_url('assets/bootstrap-table/jspdf.plugin.autotable.js'); ?>"></script>
     <script src="<?php echo base_url('assets/bootstrap-table/bootstrap-table.min.js'); ?>"></script>
     <script src="<?php echo base_url('assets/bootstrap-table/bootstrap-table-export.min.js'); ?>"></script>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
         integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous">
     </script>
-
     <nav aria-label="breadcrumb mt-sm-5">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
@@ -116,23 +105,13 @@ if($this->session->flashdata('insert_failed')){
             <li class="breadcrumb-item">Subject Attendance</li>
         </ol>
     </nav>
-
-
-
-
     <form class=" mt-5" method="post" action="<?php echo base_url();?>Professor/mark_subject_attendance">
-
         <div class="form-row mt-5">
-
-
-
             <div class="col-md-4 mb-3">
                 <label for="validationCustom07">Course</label>
-
                 <select class="custom-select" id="course" required name="course">
                     <option selected disabled value="">Choose...</option>
                     <?php 
-
 $id=$_SESSION['u_id'];
 $this->db->select('*');
 $this->db->from('users');
@@ -143,8 +122,6 @@ foreach($sql->result() as $user_data)
 {
  $dept=$user_data->dept;
 }
-
-
             $this->db->select('DISTINCT(sub_course)');
             $this->db->from('subject_assigned');
             $this->db->where('teacher_id',$_SESSION['u_id']);
@@ -162,15 +139,11 @@ foreach($sql->result() as $user_data)
                     Please enter a course.
                 </div>
             </div>
-
-
             <div class="col-md-4 mb-3">
                 <label for="validationCustom06">Subject</label>
-
                 <select class="custom-select" id="subject" required name="subject">
                     <option selected disabled value="select">Choose...</option>
                     <?php 
-
             $this->db->select('DISTINCT(subject)');
             $this->db->from('subject_assigned');
             $this->db->where('teacher_id',$_SESSION['u_id']);
@@ -181,63 +154,40 @@ foreach($sql->result() as $user_data)
             }
             ?>
                 </select>
-
             </div>
-
             <div class="col-md-4 mb-3">
                 <label for="validationCustom05">Semester</label>
-
                 <select class="custom-select" id="semester" required name="semester">
                     <option selected disabled value="">Choose...</option>
-
-
-
-
-
-
                 </select>
-
             </div>
-
             <div class=" col-12 p-0 mt-5 ">
                 <div class="col-12 col-md-12 p-0">
-
-
                     <table id="table" data-toolbar="#toolbar" data-search="false" data-sortable="false"
                         data-show-columns="false" data-toggle="table" data-pagination="false" class="table"
                         data-visible-search="false">
                         <thead class="table-primary">
-
                             <tr>
                                 <th data-field="no." data-sortable="true">no.</th>
                                 <th data-field="name" data-sortable="true">Name</th>
-
                                 <th data-field="edit">Action</th>
                             </tr>
-
                         </thead>
                         <tbody id="table_body">
                         </tbody>
                     </table>
-
-
-
                 </div>
             </div>
         </div>
         <div class="form-row mt-4">
             <input class="btn btn-primary ml-1" type="submit" name="u_reg" value="Submit">
         </div>
-
     </form>
 </div>
 <script type="text/javascript">
-
 $(document).ready(function() {
-
     $("#subject").change(function() {
         var subject = $(this).val();
-
         $.ajax({
             url: '<?php echo base_url(); ?>/professor/view_subject_ajax',
             type: 'post',
@@ -246,26 +196,54 @@ $(document).ready(function() {
             },
             dataType: 'json',
             success: function(response) {
-
                 var len = response.length;
-
                 $("#semester").empty();
                 $("#semester").append(
                     "<option disabled value='select' selected>--Select--</option>");
                 for (var i = 0; i < len; i++) {
                     var sem = response[i]['sem'];
-
-                    $("#semester").append("<option value='" + sem + "'>" + sem +"</option>");
-
+                    $("#semester").append("<option value='" + sem + "'>" + sem +
+                        "</option>");
                 }
             }
         });
     });
+
+    $("#semester").change(function() {
+        var semester = $(this).val();
+        var course = $('#course').val();
+
+        $.ajax({
+            url: '<?php echo base_url(); ?>/Professor/view_students_ajax',
+            type: 'post',
+            data: {
+                post_semester: semester,
+                post_course: course
+            },
+            dataType: 'json',
+            success: function(response) {
+
+                var len = response.length;
+                var j = 1;
+                $("#table_body").empty();
+
+                for (var i = 0; i < len; i++) {
+                    var name = response[i]['name'];
+                    var s_id = response[i]['s_id'];
+
+                    $("#table_body").append("<tr><td>" + j + "</td><td>" + name +
+                        "</td><td class=' p-0' ><input type='hidden' name='limit' value='" +
+                        j + "'><div class='form-row col-12'><input type='hidden' name='sid" + j + "' value='" +
+                        s_id + "'><div class='col-6 text-center'><input type='radio' name='v"+j+"' required value='present'> Present </div><div class='col-6 text-center'><input type='radio' name='v"+j+"' required  value='absent'> Absent </div></div></td></tr>");
+                    j++;
+
+                }
+
+
+            }
+        });
+    });
+
 });
-   
+
 </script>
-
-
-
-
-
